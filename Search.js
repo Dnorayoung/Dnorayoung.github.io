a2929d545cbb800b6608efd3e3ffc8b4269b7ac9
Dnorayoung <157241425+Dnorayoung@users.noreply.github.com>
2024-03-09 14:47:14 -0700

    Updated 3/9/24

--- Search.js
+++ Search.js
@@ -1,4 +1,17 @@
-var greenButton = About.getElementById("About");
-    console.log("About Me");
-
-
+     function myFunction() {
+     alert("You are leaving this website");
+    }
+        
+     function confirmLeave() {
+        // Display a confirmation dialog
+        var result = window.confirm("Are you sure you want to leave this website?");
+        
+        // Check the user's choice
+        if (result) {
+            // If the user clicks "OK", leave the website
+            window.location.href = "https://www.successrealtyutah.com/fine/real/estate/newsearch"; // Replace with the desired URL
+        } else {
+            // If the user clicks "Cancel", do nothing
+            return false;
+        }
+    }
